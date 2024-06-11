7ad3d04db5a9dbebaa1a499f3fde52b4045a9469
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-11 20:52:43 +0530

    First Commit

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,40 @@
+<div class="authContainer">
+    <form action="">
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email" name="" id="">
+        </label>
+        <label>
+            <input type="email" placeholder="email" name="" id="">
+        </label>
+        <label>
+            <input type="email" placeholder="email" name="" id="">
+        </label>
+    </form>
+</div>
+
+
+<style>
+    .authContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+        padding: 24px;
+    }
+    
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 400px;
+        max-width: 100%;
+        margin: 0 auto;
+    }
+
+    form input {
+        width: 100%;
+    }
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<div class="mainContainer">
+    <slot />
+</div>
+
+
+<style>
+    .mainContainer {
+        min-height: 100vh;
+        background: linear-gradient(to right, #000428, #000046);
+        color: white;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
