3b020c983aa2cb3a90d05bfa238bf52529fb1ddf
Rizwaan Ansari <108953802+RizWaaN3024@users.noreply.github.com>
2024-06-19 20:30:59 +0530

    Update firebase.js

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -7,7 +7,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCThHzCl0D8JW0lQS7WtnTcMLLw4YXyIVY",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
